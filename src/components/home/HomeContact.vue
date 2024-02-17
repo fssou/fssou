ae4aca388fdbe49a2051c2aa65aaa9187c177255
Franclin Sousa <10904895+fssou@users.noreply.github.com>
2024-02-17 20:07:59 -0300

    feat: change to telegram

--- src/components/home/HomeContact.vue
+++ src/components/home/HomeContact.vue
@@ -66,7 +66,7 @@
                     </div>
                     <button
                         class="mt-4 w-full rounded-[12px] bg-[#27272a] px-3 py-2 transition duration-1000 ease-in-out hover:bg-purple-700"
-                        @click="navigateTo(`https://wa.me/5521982385776`, { external: true, open: { target: `_blank`, }, },)"
+                        @click="navigateTo(`https://t.me/fssou`, { external: true, open: { target: `_blank`, }, },)"
                     >
                         <p class="whitespace-normal font-medium tracking-widest text-white">
                             SIM
